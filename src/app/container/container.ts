f4cac5ad2949d94f150fdd9c970ad330f4e014b5
Ebenezer Acquah <ebenezeracquah000@gmail.com>
2025-10-28 08:38:10 -0700

    fifth commit

--- src/app/container/container.ts
+++ src/app/container/container.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-container',
@@ -7,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrl: './container.css'
 })
 export class Container {
+  sum: string = "";
+  difference: number = 0;
+  division : number = 0;
+  multiplication : number = 0;
+  constructor(){}
+    ngOnInit(){
+      
+    }
+    
+/*
+  addTwoNumbers(firstNum:string, secondNum:string){
+   return parseInt(firstNum) + parseInt(secondNum);
+  }
 
+  subTwoNumbers(firstNum:string, secondNum:string){
+    if (parseInt(firstNum) > parseInt(secondNum)){
+      return parseInt(firstNum) - parseInt(secondNum);
+    }
+    return parseInt(firstNum) - parseInt(secondNum);
+  }
+
+  divTwoNumbers(firstNum:string, secondNum:string){
+    return parseInt(firstNum) / parseInt(secondNum);
+  }
+
+  multTwoNumbers(firstNum:string, secondNum:string){
+    return parseInt(firstNum) * parseInt(secondNum);
+  }
 }
+*/
+
+ addTwoNumbers(cusName:string){
+   if (parseInt(cusName) >= 18){
+    return "You are invited!";
+   }
+   return " Sorry, you're not invited!";
+  }
+}
+
